0e7b229bdf616c584fcbd177062f14346fa04a22
nyla <nyla@sajids-mbp.fios-router.home>
2021-03-23 01:39:45 -0400

    variables

---
+++ MySecondJavaProject/src/main/java/variables/Instance.java
@@ -0,0 +1,22 @@
+package variables;
+
+public class Instance {
+
+	int x=234; 
+	int y=456;
+	int z=789;
+	
+	public void fourth() {
+		
+	}
+	
+	String a="All galaxies have a supermassive black hole in its center.";
+	String b="Stars are mainly composed of hydrogen and helium.";
+	String c="The largest organ of the human body is the skin";
+	
+	public void fifth() {
+		
+	}
+
+}
+//xyz and abc are instance variables which are inside the class but outside the method.
---
+++ MySecondJavaProject/src/main/java/variables/Local.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class Local {
+
+public void first() {
+		
+		byte a=10;
+		int b = 100;
+		short c=10000;
+		long d=1234567890123456789L;	
+	}
+	
+	protected void second() {
+		
+		float e=8.12345678987654F;
+		double f=9.98765432198765432123456789;
+	}
+	
+	void third() {
+		
+		char g='N';
+		String h="I am addicted to Java.";
+	}
+
+}
