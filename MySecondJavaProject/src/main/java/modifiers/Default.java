f9e17b25a1e3ce1c6077ae0c437ff02e0b0d5a01
Sajid Huda <sajidhuda73@gmail.com>
2021-04-05 19:54:14 -0400

    methods and access modifiers

--- MySecondJavaProject/src/main/java/modifiers/Default.java
+++ MySecondJavaProject/src/main/java/modifiers/Default.java
@@ -1,14 +1,12 @@
 package modifiers;
 
 public class Default {
-
 	
 	public static void main(String[] args) {
 		EvaluateModifiers check = new EvaluateModifiers();
-		check.defaultMethod();
-		
+		check.defaultMethod();		
 	}
 }
 // Methods with 'default' access modifiers can only be accessed from
-// its own package or within its own class
+// its own package or within its own class.
 
